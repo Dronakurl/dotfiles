177f0da3931183c315fd7160d3fabfe8b80d0463
Dronakurl <konrad2710@gmail.com>
2025-02-04 18:08:19 +0100

    Add run_onchange_packages.sh

---
+++ .chezmoiscripts/run_onchange_packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Installing packages"
+
+sudo apt-get install ripgrep
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# brew install ripgrep
