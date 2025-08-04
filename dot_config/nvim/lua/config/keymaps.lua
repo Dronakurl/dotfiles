9e4a0795665862452d8c52a0f461c643b7d8dd59
Dronakurl <dronakurl@posteo.de>
2025-08-04 20:08:03 +0200

    slumber config
    slumber

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -192,3 +192,26 @@ vim.keymap.set("n", "<leader>xc", clippyme, { desc = "Apply clippy and quickfix"
 vim.keymap.set("n", "<leader>xm", mypyme, { desc = "Apply mypy and quickfix" })
 
 vim.api.nvim_set_keymap("n", "<leader>uS", ":setlocal spell spelllang=de<CR>", { noremap = false, silent = true })
+
+-- local Slumber = {}
+-- Slumber.toggle = function()
+--   local Terminal = require("toggleterm.terminal").Terminal
+--   local slumber = Terminal:new({
+--     cmd = "slumber",
+--     hidden = true,
+--     direction = "float",
+--     float_opts = {
+--       border = "none",
+--       width = 100000,
+--       height = 100000,
+--     },
+--     on_open = function(_)
+--       vim.cmd("startinsert!")
+--     end,
+--     on_close = function(_) end,
+--     count = 99,
+--   })
+--   slumber:toggle()
+-- end
+--
+-- vim.keymap.set("n", "<leader>vs", Slumber.toggle, { desc = "Open in Slumber" })
