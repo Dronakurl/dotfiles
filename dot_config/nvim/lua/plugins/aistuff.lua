799b7495d02ec508512a896ae70e4c2ca9a50333
Dronakurl <dronakurl@posteo.de>
2025-11-01 23:01:31 +0100

    claude code and properly read environment variables in fish

--- dot_config/nvim/lua/plugins/aistuff.lua
+++ dot_config/nvim/lua/plugins/aistuff.lua
@@ -46,6 +46,15 @@ local M = {
         },
       },
       adapters = {
+        acp = {
+          claude_code = function()
+            return require("codecompanion.adapters").extend("claude_code", {
+              env = {
+                CLAUDE_CODE_OAUTH_TOKEN = os.getenv("CLAUDE_CODE_OAUTH_TOKEN"),
+              },
+            })
+          end,
+        },
         mistral = function()
           return require("codecompanion.adapters").extend("mistral", {
             name = "codestral",
